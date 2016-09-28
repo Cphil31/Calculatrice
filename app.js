8148ef8394a1a779f625e5bd66d257f2fec358bb
Philippe <cecile_philippe@live.fr>
2016-09-28 22:24:31 +0200

    git init

---
+++ app.js
@@ -0,0 +1,20 @@
+var $input= $('input');
+console.log($input);
+$input.css('color','blue');
+
+// Je teste que jQuaery marche bien
+//Je récupère le texte de ma balise h1
+var valh1 =$("h1").text();
+console.log("Mon h1 est '"+valh1+"',");
+
+//si des erreurs dans les imputs.
+//afficher un message d'erreur
+
+
+//Detecter quand le formulaire est envoyé
+$("#calculer").click(clickbuttonformulaire;
+	function clickbuttonformulaire(argument) {
+		var valh1 = $("h1").text();
+		console.log("mon h1 est "+valh1+ "'.")
+	})
+console.log("calculer")
