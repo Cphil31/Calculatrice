38906a4725370732c41513c1d6bde4c2aad48293
Philippe <cecilephilippe31@gmail.com>
2017-05-11 17:43:19 +0200

    modif

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
-var $input= $('input');
-console.log($input);
-$input.css('color','blue');
+// var $input= $('input');
+// console.log($input);
+// $input.css('color','blue');
 
-// Je teste que jQuaery marche bien
-//Je récupère le texte de ma balise h1
-var valh1 =$("h1").text();
-console.log("Mon h1 est '"+valh1+"',");
+// // Je teste que jQuery marche bien
+// //Je récupère le texte de ma balise h1
+// var valh1 =$("h1").text();
+// console.log("Mon h1 est '"+valh1+"',");
 
-//si des erreurs dans les imputs.
-//afficher un message d'erreur
+// //si des erreurs dans les imputs.
+// //afficher un message d'erreur
 
 
-//Detecter quand le formulaire est envoyé
-$("#calculer").click(clickbuttonformulaire;
-	function clickbuttonformulaire(argument) {
-		var valh1 = $("h1").text();
-		console.log("mon h1 est "+valh1+ "'.")
-	})
-console.log("calculer")
+// //Detecter quand le formulaire est envoyé
+// $("#calculer").click(clickbuttonformulaire;
+// 	function clickbuttonformulaire(argument) {
+// 		var valh1 = $("h1").text();
+// 		console.log("mon h1 est "+valh1+ "'.")
+// 	})
+// console.log("calculer")
+$("#btn-1").click(
+	$("#input1").html(1);
+	);
+console.log("&");
